5100448ebf5734b7ace689d4215b98f0b25eb273
FatihSAMA <32037814+FatihSAMA@users.noreply.github.com>
2023-12-16 21:34:38 +0300

    animation

--- src/components/MobileWorks.jsx
+++ src/components/MobileWorks.jsx
@@ -1,15 +1,46 @@
 import { images } from "~/utils/images"
+import { motion } from "framer-motion"
+import { useRef } from "react"
+
+
+const container = {
+    hidden: { opacity: 1, scale: 1 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.5,
+        staggerChildren: 0.7
+      }
+    }
+  }
+    
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  }
+
+
 
 export default function MobileWorks(){
 
+    const ref = useRef()
 
     return (
-        <section id="work" className="relative bg-dark ">
+        <section id="work"  className="relative bg-dark ">
 
-            <h2 className="text-5xl pt-16 pl-8 text-light font-bold selection:text-dark">Latest Works</h2>
+            <h2  className="text-5xl pt-16 pl-8 text-light font-bold selection:text-dark">Latest Works</h2>
 
             <div className=" w-full h-fit py-16">
-                <div
+                <motion.div
+                variants={container}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once : true }}
+                
                     className="flex flex-col items-center justify-center gap-16">
                     {
                         cards.map((card) => (
@@ -18,7 +49,7 @@ export default function MobileWorks(){
                     }
 
 
-                </div>
+                </motion.div>
             </div>
         </section>
     )
@@ -29,7 +60,8 @@ export default function MobileWorks(){
 
 const Card = ({ card }) => {
     return (
-      <div
+      <motion.div
+        variants={item}
         className="group relative w-[90%] min-h-[200px] h-[300px] rounded-lg"
         style={{
             backgroundImage: `url(${card.image})`,
@@ -40,7 +72,7 @@ const Card = ({ card }) => {
         <a className=" bg-dark absolute z-10 right-3 bottom-3 rounded-2xl p-4 hover:bg-hover hover:scale-110 transition-all  " href={card.url} target="_blank" >
             <img src={images.eye} alt="" />
         </a>
-      </div>
+      </motion.div>
     );
   };
 
