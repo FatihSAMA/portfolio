6e132438ca13d533b32467838404b9e7584dd854
FatihSAMA <32037814+FatihSAMA@users.noreply.github.com>
2023-12-17 17:38:38 +0300

    portfolio

--- src/utils/images.jsx
+++ src/utils/images.jsx
@@ -5,6 +5,7 @@ import portfolio from '../assets/images/portfolio.jpg'
 import seta from '../assets/images/seta.jpg'
 import toygar from '../assets/images/toygar.jpg'
 import eye from '../assets/images/eye.svg'
+import bigarson from '../assets/images/bigarson.png'
 
 
 export const images = {
@@ -15,5 +16,6 @@ export const images = {
     seta,
     toygar,
     eye,
-        
+    bigarson,
+     
 }
